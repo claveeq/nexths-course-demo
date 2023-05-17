94900fb087c776977b390cbbfcad5ae0f42662d7
Clave Quimbo <claveeq@gmail.com>
2023-05-17 12:02:26 +0800

    add description and fix fallback

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -18,6 +18,7 @@ function MeetupItem(props) {
         <div className={classes.content}>
           <h3>{props.title}</h3>
           <address>{props.address}</address>
+          <p>{props.description}</p>
         </div>
         <div className={classes.actions}>
           <button onClick={showDetailsHanlder}>Show Details</button>
