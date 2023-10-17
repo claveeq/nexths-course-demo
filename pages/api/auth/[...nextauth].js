502fef920ff5e037c4a09850396bc355d76183e5
Clave Quimbo <claveeq@gmail.com>
2023-10-17 12:01:21 +0800

    Integrate NextAuth with Google and Github as sign-in options

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,38 +1,34 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
 
-const isCorrectCredentials = (credentials) =>
-  credentials.username === process.env.NEXTAUTH_USERNAME &&
-  credentials.password === process.env.NEXTAUTH_PASSWORD;
+import EmailProvider from "next-auth/providers/email";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
-const options = {
-  // Configure one or more authentication providers
+const prisma = new PrismaClient();
+
+export default NextAuth({
   providers: [
-    Providers.Credentials({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: "Credentials",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
       },
-      authorize: async (credentials) => {
-        if (isCorrectCredentials(credentials)) {
-          const user = { id: 1, name: "Admin" };
-          // Any object returned will be saved in `user` property of the JWT
-          return Promise.resolve(user);
-        } else {
-          // If you return null or false then the credentials will be rejected
-          return Promise.resolve(null);
-          // You can also Reject this callback with an Error or with a URL:
-          // return Promise.reject(new Error('error message')) // Redirect to error page
-          // return Promise.reject('/path/to/redirect')        // Redirect to a URL
-        }
-      },
+      from: process.env.EMAIL_FROM,
     }),
   ],
-};
-
-export default (req, res) => NextAuth(req, res, options);
+  adapter: PrismaAdapter(prisma),
+});
